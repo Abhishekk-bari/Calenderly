9121ea37ccbb0fe4c1870fc2199be3524faa247e
Abhi <abhi2bari929@gmail.com>
2024-10-12 18:09:13 +0530

    dashboard changes

--- app/utils/dashboard/page.tsx
+++ app/utils/dashboard/page.tsx
@@ -1,4 +1,13 @@
-export default function DashboardPage() {
+import { auth } from "@/app/utils/auth"
+import { redirect } from "next/navigation";
+
+export default async function DashboardPage() {
+    const session = await auth();
+
+    if(!session?.user) {
+        return redirect("/");
+    }
+
     return(
         <div>
         <h1>Hello from Dashboard</h1></div>
