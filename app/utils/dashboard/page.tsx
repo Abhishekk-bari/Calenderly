175a06e71d0e42cf418740263b2097c14ccbf27c
Abhi <abhi2bari929@gmail.com>
2024-10-12 15:28:15 +0530

    some file chnages in[ ..next]

---
+++ app/utils/dashboard/page.tsx
@@ -0,0 +1,6 @@
+export default function DashboardPage() {
+    return(
+        <div>
+        <h1>Hello from Dashboard</h1></div>
+    );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function index() {
   return (
     <div>
-     
+      <h1>Hello User</h1>
     </div>
   )
 }
